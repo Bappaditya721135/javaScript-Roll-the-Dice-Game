dae3b60cf8019803562e59442f156cfadbfb7329
Bappaditya <721135bappa@gmail.com>
2022-11-26 13:01:39 +0530

    finished current score and player switch part

--- js/scripts.js
+++ js/scripts.js
@@ -2,33 +2,41 @@
 const newGame=document.querySelector(".new-game-btn");
 const rollDice=document.querySelector(".roll-dice-btn");
 const hold=document.querySelector(".hold-btn");
-const current1=document.querySelector("#current-1");
-const current2=document.querySelector("#current-2");
 
-let currentScore=0;
+let currentScore = 0;
+let activePlayer = 0;
 
 // random dice generator
-const generateDice=()=>{
-    const imgs=document.querySelectorAll("img");
+const generateDice = ()=> {
+    const imgs = document.querySelectorAll("img");
      imgs.forEach(element => {
-        if(!element.classList.contains("not-active")){
+        if (!element.classList.contains("not-active")){
             element.classList.add("not-active");
         }
         
     });
-    let randomNumber=Math.trunc(Math.random()*6)+1;
+    let randomNumber = Math.trunc(Math.random()*6)+1;
     console.log(typeof randomNumber);
     console.log(typeof currentScore);
     const diceImg=document.querySelector(`img[data-img="${randomNumber}"]`)
     // console.log(diceImg);
 
     diceImg.classList.remove("not-active");
-    if(randomNumber==1){
+    if (randomNumber == 1) {
+        document.querySelector(`#current-${activePlayer}`).textContent = 0;
+        document.querySelector(`.player-${activePlayer}`).classList.add("active");
+        currentScore = 0;
         // change the player
+        activePlayer = activePlayer === 0? 1: 0;
+        document.querySelector(`.player-${activePlayer}`).classList.remove('active');
+        console.log(document.querySelector(`.player-${activePlayer}`));
     }
-    else{
-        currentScore+=randomNumber;
-        current1.textContent=currentScore;
+    else {
+        currentScore += randomNumber;
+        // `current${activePlayer}`.textContent = currentScore;
+        // console.log(`current${activePlayer}`);
+        // typeof `current${activePlayer}`;
+        document.querySelector(`#current-${activePlayer}`).textContent=currentScore;
     }
 }
 
